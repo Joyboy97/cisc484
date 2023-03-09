1570f4f2aafe4e7484fa3b901df1c72e3f666c20
CarlsonCarlson <carlson.cheng888@gmail.com>
2023-03-09 13:43:29 -0500

    put sum squared error in adjust weights function

--- hw1.py
+++ hw1.py
@@ -1,41 +1,53 @@
 
 
-prediction = x1*w1 + x2*w2 + x3*w3
+# prediction = x1*w1 + x2*w2 + x3*w3
 data = [
-    [2, 1, 3],
-    [5, 2, 7],
-    [2, -2, 4],
-    [-3, 1, -2],
-    [-5, 3, -1]
+    [2, 1, 3, 6],
+    [5, 2, 7, 11],
+    [2, -2, 4, 0],
+    [-3, 1, -2, 0],
+    [-5, 3, -1, 1]
     ]
 
-target_data = [6, 11, 0, 0, 1]
-# TODO: put target data back into data
-initial_weights = [.1, .2, .1, .1]
+# target_data = [6, 11, 0, 0, 1]
+initial_weights = [.2, .1, .1, .1] # w0 is on the end as .1
 weights = initial_weights
 learning_rate = .1
 # o is from y equation plugging in weights from previous epoch
 # y is calculated by plugging in 
-error = (prediction - output)
-w = wi + learning_rate(error)*x
+# error = (prediction - output)
+# w = wi + learning_rate(error)*x
 
 output = 0
 
-def sum_squared_error(data) -> int:
+# def sum_squared_error(data, weights) -> int:
+#     squared_error = 0
+#     for row in data: 
+#         prediction = row[0]*weights[1] + row[1]*weights[2] + row[2]*weights[3] + weights[0]
+#         squared_error += (prediction - row[3])**2
+#         print(squared_error)
+#     # print (squared_error)
+#     return squared_error
+
+def adjust_weight(data, weights) -> int:
     squared_error = 0
     for row in data: 
-        for y in target_data:
-            prediction = row[0]*weights[1] + row[1]*weights[2] + row[2]*weights[3] + weights[0]
-            squared_error += (prediction - y)
-    return output
-
+        prediction = row[0]*weights[1] + row[1]*weights[2] + row[2]*weights[3] + weights[0]
+        squared_error += (prediction - row[3])**2
+        print(squared_error)
+    # 1 epoch
+    for row in data:
+        for i in range(len(row) - 1):
+            weights[i] = weights[i] + learning_rate*squared_error*row[i]
+        weights[3] = weights[3] + learning_rate*squared_error*1
 
 
 
 epochs_lapsed = 0
 while (error != 0) or (epochs_lapsed < 10):
-
-
+    for weight in weights:
+        weight = weight + learning_rate*sum_squared_error(data, weights)*
+sum_squared_error(data, initial_weights)
 
 
 # go through each row in the dataset and get the err
