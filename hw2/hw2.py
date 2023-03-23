7e7873c840657f6f6ef0e16385f67cba0968e6db
CarlsonCarlson <carlson.cheng888@gmail.com>
2023-03-22 22:55:39 -0400

    all that's left is too figure out the recursion

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -20,17 +20,23 @@ def entropy_of_set(column):
     entropy = -((positive/total) * np.log2(positive/total)) - ((negative/total) * np.log2(negative/total))
     return entropy
 
-def information_gain(df, outcome_col: str, feature: str):
-    # calculate entropy for feature
-    # calculate weighted entropy for each feature
-    set_entropy = entropy_of_set(df[outcome_col])
-    # find all the 1's that also have a 1 in the outcome column
-    # find all the 1's that also have a -1 in the outcome column
+def count_pos_neg(df, feature: str, outcome_col):
     pos_mask = df.loc[(df[feature] == 1) & (df[outcome_col] == 1)]
     positives = pos_mask.shape[0]
     neg_mask = df.loc[(df[feature] == 1) & (df[outcome_col] != 1)]
     negatives = neg_mask.shape[0]
     total = positives + negatives
+    return positives, negatives, total
+
+def information_gain(df, outcome_col: str, feature: str, last_feature: str = None):
+    # calculate entropy for feature
+    # calculate weighted entropy for each feature
+    if last_feature != None:
+        # TODO: is this right
+        set_entropy = entropy_of_set(df[df[last_feature] == 1][outcome_col])
+    else:
+        set_entropy = entropy_of_set(df[outcome_col])
+    positives, negatives, total = count_pos_neg(df, feature, outcome_col)
     # print("entropy of set: " + str(set_entropy))
     # print("positives: " + str(positives))
     # print("negatives: " + str(negatives))
@@ -43,62 +49,111 @@ def information_gain(df, outcome_col: str, feature: str):
             -(positives/total) * np.log2(positives/total) - (negatives/total) * np.log2(negatives/total)))
     return information_gain
 
-# run ID3 algorithm on the dataframe
-def ID3(df, outcome_col: str):
+def max_info_gain(df, outcome_col: str):
     # find the feature with the highest information gain
     print("information gain for each feature: ")
+    info_gain_dict = {}
     for feature in df.columns:
         if feature != outcome_col:
-            print(information_gain(df, outcome_col, feature))
+            info_gain_dict.update({feature: information_gain(df, outcome_col, feature)})
     # select the feature with the highest information gain
-    # TODO: how do you represent decision tree (might not be sklearn)
+    chosen_feature = max(info_gain_dict, key=info_gain_dict.get)
+    print(info_gain_dict)
+    print(chosen_feature)
+    return chosen_feature
+
+def check_pure(positives, negatives, total):
+    if positives == 0 or negatives == 0:
+        return True
+    else:
+        return False
+
+# run ID3 algorithm on the dataframe
+def ID3(df, outcome_col: str):
+    tree = Decision_tree(None)
+    for i in range(0, 2):
+        if tree.root == None:
+            print("root:")
+            # find the feature with the highest information gain
+            chosen_feature = max_info_gain(df, outcome_col)
+            positives, negatives, total = count_pos_neg(df, chosen_feature, outcome_col)
+            print(positives, negatives, total)
+            node = Node(chosen_feature, {}, None, True)
+            if check_pure(positives, negatives, total):
+                print("pure")
+                # what's the other node become? maybe it gets set in the next call/iteration
+                if positives > negatives:
+                    node.add_attribute_child(1, "positive")
+                else:
+                    node.add_attribute_child(1, "negative")
+            node = Node(chosen_feature, {0: None, 1: None}, None, True)
+            tree.root = node
+            # remove the chosen feature from the dataframe
+            df = df.drop(columns=[chosen_feature])
+            print(df)
+        else: 
+            print("not root")
+            chosen_feature = max_info_gain(df, outcome_col)
+            positives, negatives, total = count_pos_neg(df, chosen_feature, outcome_col)
+            print(positives, negatives, total)
+            # node = Node(chosen_feature, {0: None, 1: None}, None, True)
+            # remove the chosen feature from the dataframe
+            # df = df.drop(columns=[chosen_feature])
+            print(df)
+    # populate
     return
 
 class Node:
-    def __init__(self, feature: str, attribute_to_child: dict, level: int, parent, root_bool: bool = False):
+    # def __init__(self, feature: str, attribute_to_child: dict, level: int, parent, root_bool: bool = False):
+    def __init__(self, feature: str, attribute_to_child: dict, parent, root_bool: bool = False):
         self.feature = feature
         self.attribute_to_child = attribute_to_child
         self.parent = parent
-        self.level = level
+        # self.level = level
         self.root_bool = root_bool
     
-    def __str__(self):
-        to_return = ""
-        if self.root_bool:
-            to_return += "Root: " + self.feature + "\n"
-        else:
-            to_return += "\t"*self.level + self.parent.feature + " -> " + self.feature + "\n"
+    def print_node(self, level=0):
+        to_return = "\t"*level + repr(self.feature) + "\n"
+        for attribute in self.attribute_to_child:
+            if type(self.attribute_to_child[attribute]) == str:
+                to_return += "\t"*(level+1) + repr(attribute) + " -> " + repr(self.attribute_to_child[attribute]) + "\n"
+            else: 
+                to_return += self.attribute_to_child[attribute].print_node(level+1)
+        return to_return
 
         # else: 
         #     to_return += 
         # return self.feature + 
     
+    def __repr__(self):
+        return "treenode representation"
+    
     def add_attribute_child(self, attribute: object, child):
-        self.attribute_child_dict.update({attribute: child})
+        self.attribute_to_child.update({attribute: child})
         return
     
-    # TODO: how do you increase level
 
-# class Decision_tree:
-#     def __init__(self, root: node):
-#         self.root = root
+class Decision_tree:
+    def __init__(self, root: Node):
+        self.root = root
     
-#     # print the tree
-#     def __str__(self):
-#         level = 0
-#         to_print = 
+    # print the tree
+    def __str__(self):
+        return self.root.print_node()
         
 def main():
-    # data = {"y": [1, 1, 1, 1, 1, -1, -1, -1, -1, -1],
-    #         "x1": [0, 1, 1, 0, 1, 0, 0, 0, 1, 0],
-    #         "x2": [1, 0, 1, 0, 1, 0, 0, 0, 0, 0],
-    #         "x3": [0, 1, 1, 0, 1, 1, 0, 1, 0, 1],
-    #         "x4": [1, 0, 0, 1, 0, 1, 0, 0, 0, 1]}
-    # df = pd.DataFrame(data)
-    # print(df)
-    # ID3(df, "y")
-    # node = Node("x1", {0: "x2", 1: "x3"})
-    print("thing\tthing\n"*3)
+    data = {"y": [1, 1, 1, 1, 1, -1, -1, -1, -1, -1],
+            "x1": [0, 1, 1, 0, 1, 0, 0, 0, 1, 0],
+            "x2": [1, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+            "x3": [0, 1, 1, 0, 1, 1, 0, 1, 0, 1],
+            "x4": [1, 0, 0, 1, 0, 1, 0, 0, 0, 1]}
+    df = pd.DataFrame(data)
+    print(df)
+    ID3(df, "y")
+
+    # node1 = Node("x2", {0: "x4", 1: "x4"}, None, False)
+    # node = Node("x1", {0: node1, 1: "x3"}, None, True)
+    # print(node.print_node())
     return
 
 if __name__ == "__main__":
